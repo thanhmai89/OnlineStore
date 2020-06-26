45c5fa85a130b8a5be6328ff567ee609aae2624d
Thanh Mai <phamthanhmai8998@gmail.com>
2020-06-26 07:40:06 +0700

    add users table

--- database/ecommerce1.sql
+++ database/ecommerce1.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Máy chủ: 127.0.0.1
--- Thời gian đã tạo: Th6 18, 2020 lúc 04:38 AM
+-- Thời gian đã tạo: Th6 26, 2020 lúc 02:38 AM
 -- Phiên bản máy phục vụ: 10.4.11-MariaDB
 -- Phiên bản PHP: 7.2.31
 
@@ -89,12 +89,31 @@ CREATE TABLE `product` (
 --
 
 INSERT INTO `product` (`ProductID`, `ProductName`, `CateID`, `Price`, `Quantity`, `Description`, `Picture`) VALUES
-(1, 'Iphone Xs Max', 1, 22490000, 12, 'Hoàn toàn xứng đáng với những gì được mong chờ, phiên bản cao cấp nhất iPhone Xs Max 64 GB của Apple năm nay nổi bật với chip A12 Bionic mạnh mẽ, màn hình rộng đến 6.5 inch, cùng camera kép trí tuệ nhân tạo và Face ID được nâng cấp.', './images/20200614042056iphonexmax.jpg'),
-(2, 'Dell Inspiron 3493', 3, 15490000, 7, 'Laptop Dell Inspiron 3493 i5 (N4I5136W) là chiếc máy tính xách tay văn phòng cơ bản thuộc phân khúc giá rẻ. Đây là chiếc laptop phù hợp cho học sinh, sinh viên hay nhân viên văn phòng cần một chiếc máy có cấu hình đủ dùng, thiết kế gọn nhẹ dễ dịch chuyển. ', './images/20200614042419dellinspiron.jpg'),
-(3, 'Iphone 11 Pro Max', 1, 22990000, 5, 'Chiếc iPhone mạnh mẽ nhất, lớn nhất, thời lượng pin tốt nhất hiện nay đã xuất hiện trên thị trường di động thế giới. iPhone 11 Pro Max chắc chắn là chiếc điện thoại mà ai ai cũng ao ước khi sở hữu những tính năng xuất sắc nhất, đặc biệt là camera và pin.', './images/20200614042652iphone11promax.jpg'),
-(4, 'Macbook Pro 13 inch', 3, 36590000, 2, 'MacBook Pro 13 inch được nâng cấp với bàn phím mới được thiết kế lại cho trải nghiệm gõ phím \"đã tay\" nhất từng có trên Mac, và cấu hình dung lượng cơ bản cao hơn gấp đôi, mang đến nhiều giá trị hơn cho dòng MacBook Pro vốn rất nổi tiếng.', './images/20200614042933macbookpro.jpg'),
-(5, 'iPad Air 10.5 inch', 2, 13490000, 6, 'Đã rất lâu rồi ông lớn Apple mới lại nâng cấp cho dòng iPad Air của mình và với phiên bản iPad Air 10.5 inch Wifi 2019 mới này thì thực sự người dùng đã có được một thiết bị được nâng cấp mạnh mẽ sau thời gian dài phải chờ đợi.', './images/20200614043209ipadair105.png'),
-(6, 'Samsung Tab s6', 2, 18490000, 9, 'Samsung Galaxy Tab S6 là chiếc máy tính bảng 2 trong 1, được thiết kế để giúp cho những người cần một sản phẩm với mức giá phải chăng và cấu hình tốt phù hợp với nhu cầu sử dụng của mọi lứa tuổi khác nhau.', './images/20200614043441samsungtabs6.jpg');
+(25, 'Dell Inspiron 3493', 3, 15490000, 8, 'Laptop Dell Inspiron 3493 i5 (N4I5136W) là chiếc máy tính xách tay văn phòng cơ bản thuộc phân khúc giá rẻ. Đây là chiếc laptop phù hợp cho học sinh, sinh viên hay nhân viên văn phòng cần một chiếc máy có cấu hình đủ dùng, thiết kế gọn nhẹ dễ dịch chuyển.', './images/20200625024806dellinspiron.jpg'),
+(26, 'Macbook Pro 13 inch', 3, 36590000, 3, 'MacBook Pro 13 inch được nâng cấp với bàn phím mới được thiết kế lại cho trải nghiệm gõ phím \"đã tay\" nhất từng có trên Mac, và cấu hình dung lượng cơ bản cao hơn gấp đôi, mang đến nhiều giá trị hơn cho dòng MacBook Pro vốn rất nổi tiếng.', './images/20200625024818macbookpro.jpg'),
+(27, 'Iphone 11 Pro Max', 1, 22990000, 6, 'Chiếc iPhone mạnh mẽ nhất, lớn nhất, thời lượng pin tốt nhất hiện nay đã xuất hiện trên thị trường di động thế giới. iPhone 11 Pro Max chắc chắn là chiếc điện thoại mà ai ai cũng ao ước khi sở hữu những tính năng xuất sắc nhất, đặc biệt là camera và pin.', './images/20200625024828iphone11promax.jpg'),
+(28, 'iPad Air 10.5 inch', 2, 13490000, 6, 'Đã rất lâu rồi ông lớn Apple mới lại nâng cấp cho dòng iPad Air của mình và với phiên bản iPad Air 10.5 inch Wifi 2019 mới này thì thực sự người dùng đã có được một thiết bị được nâng cấp mạnh mẽ sau thời gian dài phải chờ đợi.', './images/20200625050348ipadair105.png'),
+(29, 'Samsung Tab S6', 2, 18490000, 10, 'Samsung Galaxy Tab S6 là chiếc máy tính bảng 2 trong 1, được thiết kế để giúp cho những người cần một sản phẩm với mức giá phải chăng và cấu hình tốt phù hợp với nhu cầu sử dụng của mọi lứa tuổi khác nhau.', './images/20200625050432samsungtabs6.jpg');
+
+-- --------------------------------------------------------
+
+--
+-- Cấu trúc bảng cho bảng `users`
+--
+
+CREATE TABLE `users` (
+  `UserID` int(5) NOT NULL,
+  `UserName` varchar(30) NOT NULL,
+  `Email` varchar(30) NOT NULL,
+  `Password` varchar(50) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+--
+-- Đang đổ dữ liệu cho bảng `users`
+--
+
+INSERT INTO `users` (`UserID`, `UserName`, `Email`, `Password`) VALUES
+(3, 'TuocGia', 'phamthanhmai0809@gmail.com', 'bdde3009dbfd1e452cd5df05279e1e56');
 
 --
 -- Chỉ mục cho các bảng đã đổ
@@ -127,6 +146,12 @@ ALTER TABLE `product`
   ADD KEY `CateID` (`CateID`);
 
 --
+-- Chỉ mục cho bảng `users`
+--
+ALTER TABLE `users`
+  ADD PRIMARY KEY (`UserID`);
+
+--
 -- AUTO_INCREMENT cho các bảng đã đổ
 --
 
@@ -146,7 +171,13 @@ ALTER TABLE `orderproduct`
 -- AUTO_INCREMENT cho bảng `product`
 --
 ALTER TABLE `product`
-  MODIFY `ProductID` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=7;
+  MODIFY `ProductID` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=30;
+
+--
+-- AUTO_INCREMENT cho bảng `users`
+--
+ALTER TABLE `users`
+  MODIFY `UserID` int(5) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=5;
 
 --
 -- Các ràng buộc cho các bảng đã đổ
